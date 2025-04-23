ca6408d134198bfc43051118fa91e62eabb4a0e4
labdhishah23 <labdhimanish4@gmail.com>
2025-04-23 14:00:12 +0530

    Delete lab 11.py

--- lab 11.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        number = int(input("Enter an integer: "))
-        print(f"You entered: {number}")
-        break
-    except ValueError:
-        print("Invalid input! Please enter a valid integer.")
